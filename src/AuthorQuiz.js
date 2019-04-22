cb41163dc45f26c90e140edbab881fd5ca646778
James Payne <jwepayne@hotmail.co.uk>
2019-04-22 22:44:27 +0100

    Routing working as needed

---
+++ src/AuthorQuiz.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Link, withRouter } from 'react-router-dom';
+import './App.css'; 
+import './bootstrap.min.css'
+
+
+
+
+function Hero() {
+  return (<div className="row">
+    <div className="jumbotron col-10 offset-1">
+      <h1>Author Quiz</h1>
+      <p>Select the book written by the author shown</p>
+    </div> 
+  </div>);
+}
+
+function Book({title, onClick}){
+  return (<div className="answer" onClick={() => {onClick(title)}}>
+    <h4>{title}</h4>
+  </div> 
+  );
+}
+
+function Turn({author, books, highlight, onAnswerSelected}) {
+  function highlightToBgColor(highlight){
+    const mapping = {
+      'none': '',
+      'correct': 'green',
+      'wrong': 'red'
+    };
+    return mapping[highlight];
+  }
+
+  return (<div className="row turn" style={{backgroundColor: highlightToBgColor(highlight)}}>
+    <div className="col-4 offset-1">
+      <img src={author.imageUrl} className="authorimage" alt="Author"/>     
+    </div> 
+    <div className="col-6">
+      {books.map((title) => <Book title={title} key={title} onClick={onAnswerSelected}/>)}
+    </div>  
+  </div>);
+}
+
+
+function Continue() {
+  return <div/>
+}
+function Footer() {
+  return (<div id="footer" className="row">
+    <div className="col-12">
+      <p className="text-muted credit">
+        All images are frrom <a href="http://commons.wikimedia.org/wiki/Main_Page">Wikemedia Commons</a> and are in the public domain
+      </p>
+    </div>
+  </div>);
+}
+function AuthorQuiz({turnData, highlight, onAnswerSelected}) {
+  return (
+      <div className="container-fluid">
+    
+        <Hero/>
+        <Turn {...turnData} highlight={highlight} onAnswerSelected={onAnswerSelected} />
+        <Continue/>
+   
+            <Link to="/Add">Add New Author</Link>
+          
+        <Footer/>
+        
+      </div>
+     
+    );
+  }
+
+
+export default AuthorQuiz;
+
